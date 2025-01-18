cf320033e2bed202df1b2218139ab1f274fad40e
steeezyro <rohanchhaya@gmail.com>
2025-01-17 17:59:13 -0800

    Prepare backend for deployment on Render

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # We can comment this CORS config for the production because we are running the frontend and backend on the same server
 # CORS(app) 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///friends.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
